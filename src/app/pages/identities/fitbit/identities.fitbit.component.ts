0c102616f6984ddaed7e4ffc78f287d86ee416ea
fabrizio748s <fabri_occhio@hotmail.it>
2018-03-02 13:11:45 +0100

    Added fitbit pages and services

--- src/app/pages/identities/fitbit/identities.fitbit.component.ts
+++ src/app/pages/identities/fitbit/identities.fitbit.component.ts
@@ -1,26 +1,154 @@
-import {Component} from '@angular/core';
-import {environment} from '../../../../environments/environment';
+import {Component, OnInit} from '@angular/core';
+import {FitbitService} from '../../../services/fitbit.service';
 import {isNullOrUndefined} from 'util';
 import {ToastrService} from 'ngx-toastr';
-import {FitbitService} from '../../../services/fitbit.service';
+import {ActivatedRoute} from '@angular/router';
+import {AuthService} from '../../../services/auth.service';
+import {MatDialog, MatTableDataSource} from '@angular/material';
+import {ConfirmDialogComponent} from '../../../components/confirm-dialog/confirm-dialog.component';
+import {environment} from '../../../../environments/environment';
+
+const DELAY_TIMEOUT = 3500; // milliseconds
 
 @Component({
-  templateUrl: './identities.fitbit.component.html'
+  styleUrls: ['./identities.fitbit.component.scss'],
+  templateUrl: './identities.fitbit.component.html',
 })
-export class IdentitiesFitbitComponent {
+export class IdentitiesFitbitComponent implements OnInit {
+
+  /**
+   * Current logged user.
+   */
+  user: any;
+
+  /**
+   * True if something is loading.
+   */
+  loading = true;
+
+  /**
+   * True if posts are loading.
+   */
+  loadingPosts = false;
+
+  /**
+   * True if likes are loading.
+   */
+  loadingLikes = false;
+
+  /**
+   * True if friends are loading.
+   */
+  loadingFriends = false;
+
+  /**
+   * Posts array.
+   */
+  posts: any[] = [];
+
+  /**
+   * Likes array.
+   */
+  likes: any[] = [];
+
+  /**
+   * Friends array.
+   */
+  friends: any[] = [];
+
+  /**
+   * Share profile option.
+   */
+  shareProfile: boolean;
+
+  /**
+   * Share messages option.
+   */
+  shareMessages: boolean;
+
+  /**
+   * Share friends option.
+   */
+
+  shareFriends: boolean;
+
+  /**
+   * Share likes option.
+   */
+  shareLikes: boolean;
 
   /**
    * Application name.
    */
   appName: string;
 
+  // data source containing user Fitbit profile data
+  dataSource: MatTableDataSource<any>;
+  displayedColumns = ['dataName', 'dataValue'];
+
+  // variable returned by Fitbit and used to complete association process
+  private authorizationCode: string;
+
   constructor(
+    private fitbitService: FitbitService,
     private toast: ToastrService,
-    private fitbitService: FitbitService
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private dialog: MatDialog,
   ) {
     this.appName = environment.appName;
   }
 
+  /**
+   * @override
+   */
+  ngOnInit(): void {
+
+    this.authService.getUser().then((user) => {
+      if (user && user.identities && user.identities.fitbit) {
+
+        this.loading = false;
+        this.user = user;
+        this.setupFitbitProfileTable();
+       /*  this.updatePosts(10);*/
+        /* this.updateLikes(10);*/
+        /* this.updateFriends(10);*/
+
+        // set share values
+        this.shareProfile = this.user.identities.configs.fitbitConfig.shareProfile;
+      /*  this.shareMessages = this.user.identities.configs.fitbitConfig.shareMessages;
+        this.shareFriends = this.user.identities.configs.fitbitConfig.shareFriends;
+        this.shareLikes = this.user.identities.configs.fitbitConfig.shareLikes;*/
+
+        // clean the URL
+        window.history.replaceState(null, null, window.location.pathname);
+
+      } else {
+
+
+        // reading parameters returned by Fitbit
+        this.route.queryParams.subscribe(params => {
+          this.authorizationCode = params['code'];
+
+          if (this.authorizationCode) {
+
+            // clean the URL
+            window.history.replaceState(null, null, window.location.pathname);
+
+            // request access token
+            this.fitbitService.accessToken(this.authorizationCode).subscribe((res) => {
+
+              // update Fitbit profile
+              this.updateProfile();
+
+            });
+          } else {
+            this.loading = false;
+          }
+        });
+      }
+    });
+  }
 
   /**
    * Associate user account with Fitbit.
@@ -28,7 +156,7 @@ export class IdentitiesFitbitComponent {
   associate() {
     this.fitbitService.getLoginDialog().subscribe(
       (res) => {
-        window.location.href = res.redirectUrl;
+        window.location.href = res.loginDialogUrl;
       },
       (err) => {
         if (!isNullOrUndefined(err.error.message)) {
@@ -39,4 +167,155 @@ export class IdentitiesFitbitComponent {
       });
   }
 
+  /**
+   * Update user Fitbit profile information.
+   * @param showToast: if you want to show the toast messages
+   */
+  updateProfile(showToast?: boolean) {
+
+    this.fitbitService.profile().subscribe((res) => {
+      this.loading = false;
+      if (res && res.user) {
+        if (showToast) {
+          this.toast.success('Profile Updated');
+        }
+        this.user = res.user;
+
+        // set share values
+        this.shareProfile = this.user.identities.configs.fitbitConfig.shareProfile;
+
+        this.setupFitbitProfileTable();
+      } else {
+        if (showToast) {
+          this.toast.warning('Timeout not elapsed. Retry in about five minutes');
+        }
+      }
+    });
+  }
+
+  /**
+   * Update user Posts.
+   * @param messagesToRead: the messages number to retrieve
+   * @param showToast: if you want to show the toast messages
+   */
+
+  /*TODO*/
+
+
+  /**
+   * Update user likes.
+   * @param likesToRead: the likes number to retrieve
+   * @param showToast: if you want to show the toast messages
+   */
+
+  /*TODO*/
+
+
+  /**
+   * Update user friends.
+   * @param friendsToRead: the friends number to retrieve
+   * @param showToast: if you want to show the toast messages
+   */
+
+  /*TODO*/
+
+
+  /**
+   * Update share profile.
+   */
+  updateShareProfile() {
+    this.fitbitService.configuration({shareProfile: this.shareProfile}).subscribe((res) => {
+      if (res && res.auth) {
+        this.toast.success('Configuration updated');
+      } else {
+        this.toast.error('An error occurred');
+      }
+    });
+  }
+
+  /**
+   * Update share messages.
+   */
+  updateShareMessages() {
+    this.fitbitService.configuration({shareMessages: this.shareMessages}).subscribe((res) => {
+      if (res && res.auth) {
+        this.toast.success('Configuration updated');
+      } else {
+        this.toast.error('An error occurred');
+      }
+    });
+  }
+
+  /**
+   * Update share friends.
+   */
+  updateShareFriends() {
+    this.fitbitService.configuration({shareFriends: this.shareFriends}).subscribe((res) => {
+      if (res && res.auth) {
+        this.toast.success('Configuration updated');
+      } else {
+        this.toast.error('An error occurred');
+      }
+    });
+  }
+
+
+  /**
+   * Update share likes.
+   */
+  updateShareLikes() {
+    this.fitbitService.configuration({shareLikes: this.shareLikes}).subscribe((res) => {
+      if (res && res.auth) {
+        this.toast.success('Configuration updated');
+      } else {
+        this.toast.error('An error occurred');
+      }
+    });
+  }
+
+  /**
+   * Delete Fitbit information account, including posts and likes.
+   */
+  deleteAccount() {
+    const dialog = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        infoText: 'Are you sure? All data related to your Fitbit account will be deleted from ' + environment.appName
+      }
+    });
+
+    dialog.afterClosed().subscribe(result => {
+      if (result) {
+        this.fitbitService.deleteAccount().subscribe((res) => {
+          if (res.auth) {
+            this.user.identities.fitbit = null;
+          } else {
+            this.toast.error('Something went wrong.');
+          }
+        });
+      }
+    });
+  }
+
+  /**
+   * Populate the dataSource object reading the element from user Fitbit profile.
+   */
+  private setupFitbitProfileTable() {
+
+    // user Fitbit data
+    const fitbit = this.user.identities.fitbit;
+
+    // array used to populate the data source object
+    const fitbitProfile: {dataName: string, dataValue: any}[] = [];
+    fitbitProfile.push({dataName: 'Full Name', dataValue: fitbit['fullName']});
+    fitbitProfile.push({dataName: 'aboutMe', dataValue: fitbit['aboutMe']});
+    fitbitProfile.push({dataName: 'Gender', dataValue: fitbit['gender']});
+    fitbitProfile.push({dataName: 'country', dataValue: fitbit['country']});
+    fitbitProfile.push({dataName: 'weight', dataValue: fitbit['weight']});
+    fitbitProfile.push({dataName: 'state', dataValue: fitbit['state']});
+    fitbitProfile.push({dataName: 'dateOfBirth', dataValue: fitbit['dateOfBirth']});
+    fitbitProfile.push({dataName: 'height', dataValue: fitbit['height']});
+    fitbitProfile.push({dataName: 'city', dataValue: fitbit['city']});
+    fitbitProfile.push({dataName: 'displayName', dataValue: fitbit['displayName']});
+    this.dataSource = new MatTableDataSource(fitbitProfile);
+  }
 }
--- src/app/services/fitbit.service.ts
+++ src/app/services/fitbit.service.ts
@@ -2,21 +2,35 @@ import {Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
+import {isNullOrUndefined} from 'util';
 
-const API_LOGIN_DIALOG = 'api/facebook/login_dialog';
+const API_LOGIN_DIALOG = 'api/fitbit/login_dialog';
+const API_REQUEST_TOKEN = 'api/fitbit/request_token';
+const API_USER_PROFILE = 'api/fitbit/profile';
+const API_DELETE_ACCOUNT = 'api/fitbit/delete';
+const API_CONFIG = 'api/fitbit/config';
 
+const FIVE_MINUTES_MILLIS = 5 * 60 * 1000;
 
 @Injectable()
 export class FitbitService {
 
   private url: string;
 
+  // timeout variables
+  private lastUpdateProfile: number;
+  private lastUpdatePosts: number;
+  private lastUpdateLikes: number;
+  private lastUpdateFriends: number;
 
   constructor(
     private http: HttpClient,
   ) {
     this.url = environment.api;
+    this.lastUpdateProfile = Date.now() - FIVE_MINUTES_MILLIS;
+    this.lastUpdatePosts = Date.now() - FIVE_MINUTES_MILLIS;
+    this.lastUpdateLikes = Date.now() - FIVE_MINUTES_MILLIS;
+    this.lastUpdateFriends = Date.now() - FIVE_MINUTES_MILLIS;
   }
 
   /**
@@ -30,4 +44,95 @@ export class FitbitService {
     return this.http.post(`${this.url}${API_LOGIN_DIALOG}`, postParams);
   }
 
+  /**
+   * Get the access token.
+   * @param authorizationCode: authorization code returned by Fitbit
+   * @return{Observable<Object>}: Fitbit user accessToken
+   */
+  accessToken(authorizationCode: string): Observable<any> {
+    const postParams = {
+      code: authorizationCode,
+      callbackUrl: environment.fitbitCallbackUrl,
+    };
+    return this.http.post(`${this.url}${API_REQUEST_TOKEN}`, postParams);
+  }
+
+  /**
+   * Get user profile information.
+   * @return{Observable<Object>}: Fitbit user profile information if request was sent, false otherwise
+   */
+  profile(): Observable<any> {
+    // timeout
+    if (Date.now() - this.lastUpdateProfile >= FIVE_MINUTES_MILLIS) {
+      this.lastUpdateProfile = Date.now();
+      return this.http.get(`${this.url}${API_USER_PROFILE}`);
+    } else {
+      return Observable.of(false);
+    }
+  }
+
+  /**
+   * Get user posts.
+   * @param messagesToRead: the number of messages to retrieve from database. If not specified, update the user messages
+   *        with new ones
+   * @return{Observable<Object>}: Fitbit user posts if request was sent, false otherwise
+   */
+
+
+ /*TODO*/
+
+  /**
+   * Get user likes.
+   * @param likesToRead the number of likes to retrieve from database. If not specified, update the user likes
+   *        with new ones
+   * @return {Observable<Object>}: Fitbit user likes if request was sent, false otherwise
+   */
+
+
+  /*TODO*/
+
+
+  /**
+   * Get user friends.
+   * @param friendsToRead the number of friends to retrieve from database. If not specified, update the user friends
+   * @return {Observable<Object>}: Fitbit user friends if request was sent, false otherwise
+   */
+
+  /*TODO*/
+
+
+  /**
+   * Send Fitbit configuration to update.
+   * @param option: share options
+   * @return {Observable<Object>}
+   */
+  configuration(option: {shareProfile?: boolean, shareMessages?: boolean, shareFriends?: boolean, shareLikes?: boolean}): Observable<any> {
+    let params = '?';
+
+    if (!isNullOrUndefined(option.shareProfile)) {
+      params += 'shareProfile=' + option.shareProfile + '&';
+    }
+
+    if (!isNullOrUndefined(option.shareMessages)) {
+      params += 'shareMessages=' + option.shareMessages + '&';
+    }
+
+    if (!isNullOrUndefined(option.shareFriends)) {
+      params += 'shareFriends=' + option.shareFriends + '&';
+    }
+
+    if (!isNullOrUndefined(option.shareLikes)) {
+      params += 'shareLikes=' + option.shareLikes + '&';
+    }
+
+    return this.http.get(`${this.url}${API_CONFIG}${params}`);
+  }
+
+  /**
+   * Delete user Fitbit account.
+   * @return {Observable<Object>}
+   */
+  deleteAccount(): Observable<any> {
+    return this.http.delete(`${this.url}${API_DELETE_ACCOUNT}`);
+  }
 }
